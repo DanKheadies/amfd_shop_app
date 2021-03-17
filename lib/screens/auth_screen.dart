0b39f450641c1635457da63086b4d8e8e8499d34
Papa Daco <papadaco@PapaDaco.local>
2021-03-17 15:07:13 -0400

    S12 Animations -- Details AniAppBar opt, et al.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -106,7 +106,8 @@ class _AuthCardState extends State<AuthCard>
   var _isLoading = false;
   final _passwordController = TextEditingController();
   AnimationController _controller;
-  Animation<Size> _heightAnimation;
+  Animation<Offset> _slideAnimation;
+  Animation<double> _opacityAnimation;
 
   @override
   void initState() {
@@ -117,19 +118,24 @@ class _AuthCardState extends State<AuthCard>
         milliseconds: 300,
       ),
     );
-    _heightAnimation = Tween<Size>(
-      begin: Size(
-        double.infinity,
-        260,
-      ),
-      end: Size(
-        double.infinity,
-        320,
-      ),
+
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0, -1.5),
+      end: Offset(0, 0),
     ).animate(
       CurvedAnimation(
         parent: _controller,
         curve: Curves.fastOutSlowIn,
+      ),
+    );
+
+    _opacityAnimation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeIn,
       ),
     );
     // _heightAnimation.addListener(() => setState(() {}));
@@ -218,13 +224,13 @@ class _AuthCardState extends State<AuthCard>
     if (_authMode == AuthMode.Login) {
       setState(() {
         _authMode = AuthMode.Signup;
-        _controller.forward();
       });
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
-        _controller.reverse();
       });
+      _controller.reverse();
     }
   }
 
@@ -236,22 +242,19 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      // child: AnimatedBuilder(
-      //   animation: _heightAnimation,
-      //   builder: (ctx, ch) => Container(
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, ch) => Container(
-          // height: _authMode == AuthMode.Signup ? 320 : 260,
-          height: _heightAnimation.value.height,
-          constraints: BoxConstraints(
-            // minHeight: _authMode == AuthMode.Signup ? 320 : 260,
-            minHeight: _heightAnimation.value.height,
-          ),
-          width: deviceSize.width * 0.75,
-          padding: EdgeInsets.all(16.0),
-          child: ch,
+      child: AnimatedContainer(
+        duration: Duration(
+          milliseconds: 300,
         ),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints: BoxConstraints(
+          minHeight: _authMode == AuthMode.Signup ? 320 : 260,
+          // minHeight: _heightAnimation.value.height,
+        ),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -284,20 +287,36 @@ class _AuthCardState extends State<AuthCard>
                     _authData['password'] = value;
                   },
                 ),
-                if (_authMode == AuthMode.Signup)
-                  TextFormField(
-                    enabled: _authMode == AuthMode.Signup,
-                    decoration: InputDecoration(labelText: 'Confirm Password'),
-                    obscureText: true,
-                    validator: _authMode == AuthMode.Signup
-                        ? (value) {
-                            if (value != _passwordController.text) {
-                              return 'Passwords do not match!';
-                            }
-                            return null;
-                          }
-                        : null,
+                AnimatedContainer(
+                  duration: Duration(
+                    milliseconds: 300,
                   ),
+                  constraints: BoxConstraints(
+                    minHeight: _authMode == AuthMode.Signup ? 60 : 0,
+                    maxHeight: _authMode == AuthMode.Signup ? 120 : 0,
+                  ),
+                  curve: Curves.easeIn,
+                  child: FadeTransition(
+                    opacity: _opacityAnimation,
+                    child: SlideTransition(
+                      position: _slideAnimation,
+                      child: TextFormField(
+                        enabled: _authMode == AuthMode.Signup,
+                        decoration:
+                            InputDecoration(labelText: 'Confirm Password'),
+                        obscureText: true,
+                        validator: _authMode == AuthMode.Signup
+                            ? (value) {
+                                if (value != _passwordController.text) {
+                                  return 'Passwords do not match!';
+                                }
+                                return null;
+                              }
+                            : null,
+                      ),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -14,47 +14,102 @@ class ProductDetailScreen extends StatelessWidget {
       listen: false,
     ).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title),
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title),
+      // ),
+      // body: SingleChildScrollView(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title),
+              background: Hero(
+                tag: loadedProduct.id,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '\$${loadedProduct.price}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    loadedProduct.description,
+                    textAlign: TextAlign.center,
+                    softWrap: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 800,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                loadedProduct.imageUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 10,
-              ),
-              width: double.infinity,
-              child: Text(
-                loadedProduct.description,
-                textAlign: TextAlign.center,
-                softWrap: true,
-              ),
-            ),
-          ],
-        ),
-      ),
+      // child: Column(
+      //   children: [
+      //     Container(
+      //       height: 300,
+      //       width: double.infinity,
+      //       child: Hero(
+      //         tag: loadedProduct.id,
+      //         child: Image.network(
+      //           loadedProduct.imageUrl,
+      //           fit: BoxFit.cover,
+      //         ),
+      //       ),
+      //     ),
+      // SizedBox(
+      //   height: 10,
+      // ),
+      // Text(
+      //   '\$${loadedProduct.price}',
+      //   style: TextStyle(
+      //     color: Colors.grey,
+      //     fontSize: 20,
+      //   ),
+      // ),
+      // SizedBox(
+      //   height: 10,
+      // ),
+      // Container(
+      //   padding: EdgeInsets.symmetric(
+      //     horizontal: 10,
+      //   ),
+      //   width: double.infinity,
+      //   child: Text(
+      //     loadedProduct.description,
+      //     textAlign: TextAlign.center,
+      //     softWrap: true,
+      //   ),
+      // ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
